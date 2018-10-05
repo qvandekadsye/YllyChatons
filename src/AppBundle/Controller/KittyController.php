3ced65789e7a954df57d5d8d8048eed647d59057
qvandekadsye <qvandekadsye@gmail.com>
2018-10-05 14:23:32 +0200

    Gestion DELETE

--- src/AppBundle/Controller/KittyController.php
+++ src/AppBundle/Controller/KittyController.php
@@ -7,8 +7,8 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Kitty;
 use AppBundle\Repository\KittyRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -18,10 +18,12 @@ use Symfony\Component\HttpFoundation\Response;
 class KittyController extends Controller
 {
     protected $kittyRepository;
+    protected $entityManager;
 
-    public function __construct(KittyRepository $kittyRepository)
+    public function __construct(KittyRepository $kittyRepository, EntityManagerInterface $entityManager)
     {
         $this->kittyRepository = $kittyRepository;
+        $this->entityManager = $entityManager;
     }
 
 
@@ -54,8 +56,26 @@ class KittyController extends Controller
     /**
      * * @Rest\Get("/api/kitties/")
      */
-    public function RedirectToGetKittiesAction()
+    public function redirectToGetKittiesAction()
     {
         return $this->redirectToRoute('get_kitties');
+    }
+
+    /**
+     * @param Request $request
+     * @Rest\View()
+     * @Rest\Delete("/api/kitties/{id}")
+     *
+     * @return JsonResponse
+     */
+    public function deleteKittyAction(Request $request)
+    {
+        $kitty = $this->kittyRepository->find($request->get('id'));
+        if (empty($kitty)) {
+            return new JsonResponse(array('message' => 'Kitty not found'), Response::HTTP_NOT_FOUND);
+        }
+        $this->entityManager->remove($kitty);
+        $this->entityManager->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
